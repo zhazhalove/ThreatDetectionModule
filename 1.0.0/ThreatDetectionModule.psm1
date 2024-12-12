bbffb488d29be93db89ab24d8e92522f7fe2cf86
zhazhalove <zhazhalove@gmail.com>
2024-12-12 04:14:31 -0500

    refactored core functions

--- 1.0.0/ThreatDetectionModule.psm1
+++ 1.0.0/ThreatDetectionModule.psm1
@@ -118,6 +118,36 @@ function Test-InputMessage {
 }
 
 
+<#
+.SYNOPSIS
+Sets the MAMBA_ROOT_PREFIX environment variable for micromamba.
+
+.DESCRIPTION
+The `Initialize-MambaRootPrefix` function sets the environment variable `MAMBA_ROOT_PREFIX` to the specified path for micromamba. If no path is specified, it defaults to the `$env:APPDATA\micromamba` directory. This environment variable is typically used by micromamba to define the root directory for its environments.
+
+.PARAMETER MAMBA_ROOT_PREFIX
+The path to set for the `MAMBA_ROOT_PREFIX` environment variable. If not provided, the function defaults to `$env:APPDATA\micromamba`.
+
+.EXAMPLE
+Initialize-MambaRootPrefix -MAMBA_ROOT_PREFIX "C:\mamba"
+This will set the `MAMBA_ROOT_PREFIX` environment variable to "C:\mamba".
+
+.EXAMPLE
+Initialize-MambaRootPrefix
+This will set the `MAMBA_ROOT_PREFIX` environment variable to the default value of `$env:APPDATA\micromamba`.
+
+.NOTES
+https://mamba.readthedocs.io/en/latest/user_guide/concepts.html#root-prefix
+#>
+function Initialize-MambaRootPrefix {
+    param (
+        [Parameter(Mandatory = $false)]
+        [string]$MAMBA_ROOT_PREFIX = "$env:APPDATA\micromamba"
+    )
+    # Set MAMBA_ROOT_PREFIX environment variable
+    $env:MAMBA_ROOT_PREFIX = $MAMBA_ROOT_PREFIX
+}
+
 
 <#
 .SYNOPSIS
@@ -152,69 +182,133 @@ function Test-MicromambaEnvironment {
 .PARAMETER Packages
     A list of package names to be installed.
 
+.PARAMETER TrustedHost
+    A switch parameter to trust the certificate for pip installation. 
+    If provided, adds the --trusted-host flag to the pip install command for trusted hosts (pypi.org and files.pythonhosted.org).
+    https://pip.pypa.io/en/stable/topics/https-certificates/#
+
 .EXAMPLE
     Install-PackagesInMicromambaEnvironment -EnvName "langchain" -Packages @("numpy", "pandas", "matplotlib")
+
+.EXAMPLE
+Install-PackagesInMicromambaEnvironment -EnvName "langchain" -Packages @("numpy", "pandas") -TrustedHost
 #>
 function Install-PackagesInMicromambaEnvironment {
     param (
         [Parameter(Mandatory = $true)]
         [string]$EnvName,
+
         [Parameter(Mandatory = $true)]
-        [string[]]$Packages
+        [string[]]$Packages,
+
+        [Parameter()]
+        [switch]$TrustedHost
     )
 
-    Write-Host "Installing package in micromamba environment: $EnvName" -ForegroundColor Yellow
+    # Initialize an array to store results
+    [PSCustomObject[]]$results = @()
+
+    # Write-Host "Installing packages in micromamba environment: $EnvName" -ForegroundColor Yellow
 
     foreach ($package in $Packages) {
-        Write-Host "Installing package: $package" -ForegroundColor Green
-        # redirect output since uipath captures the output
-        & "$PSScriptRoot\micromamba" run -n $EnvName pip install $package | Out-Null
+        # Write-Host "Installing package: $package" -ForegroundColor Green
 
-        if ($LASTEXITCODE -ne 0) {
-            Write-Host "Failed to install package: $package" -ForegroundColor Red
-            break
+        try {
+            if ($TrustedHost) {
+                # Run the install command with trusted hosts
+                & "$PSScriptRoot\micromamba" run -n $EnvName pip install $package --trusted-host pypi.org --trusted-host files.pythonhosted.org | Out-Null
+            }
+            else {
+                # Run the install command without trusted hosts
+                & "$PSScriptRoot\micromamba" run -n $EnvName pip install $package | Out-Null
+            }
+
+            # Check if the installation was successful
+            if ($LASTEXITCODE -eq 0) {
+                # Add success result for this package
+                $results += [PSCustomObject]@{
+                    PackageName = $package
+                    Success     = $true
+                }
+                # Write-Host "Successfully installed package: $package" -ForegroundColor Cyan
+            }
+            else {
+                # Add failure result for this package
+                $results += [PSCustomObject]@{
+                    PackageName = $package
+                    Success     = $false
+                }
+                # Write-Host "Failed to install package: $package" -ForegroundColor Red
+            }
+        }
+        catch {
+            # In case of an exception, log the failure for this package
+            $results += [PSCustomObject]@{
+                PackageName = $package
+                Success     = $false
+            }
+            # Write-Host "Error installing package: $package" -ForegroundColor Red
         }
     }
 
-    Write-Host "Package installation complete for environment: $EnvName" -ForegroundColor Yellow
+    # Write-Host "Package installation complete for environment: $EnvName" -ForegroundColor Yellow
+
+    return $results
 }
+
 
 
 <#
 .SYNOPSIS
-    Initializes the micromamba environment.
+    Creates the micromamba environment.
 
 .PARAMETER EnvName
-    The name of the micromamba environment to create.
+    The name of the micromamba environment to create. Required
 
 .PARAMETER PythonVersion
-    The Python version to use for the environment.
+    The Python version to use for the environment. Defaults to 3.11
 
-.PARAMETER Packages
-    A string[] list of python packages to install into the mircomamba environment.
-
+.PARAMETER TrustedHost
+    A switch parameter to trust the certificate for pip installation. 
+    If provided, adds the --trusted-host flag to the pip install command for trusted hosts (pypi.org and files.pythonhosted.org).
+    https://pip.pypa.io/en/stable/topics/https-certificates/#
 
 .EXAMPLE
-    Initialize-MicromambaEnvironment -EnvName "langchain" -PythonVersion "3.11" -Packages @("numpy", "pandas", "scipy")
+    Initialize-MicromambaEnvironment -EnvName "langchain"
+
+.EXAMPLE
+    Initialize-MicromambaEnvironment -EnvName "langchain" -PythonVersion "3.11" -TrustedHost
 #>
-function Initialize-MicromambaEnvironment {
+function New-MicromambaEnvironment {
     param (
         [Parameter(Mandatory = $true)]
         [string]$EnvName,
-        [Parameter(Mandatory = $true)]
-        [string]$PythonVersion,
+
         [Parameter(Mandatory = $false)]
-        [string[]]$Packages
+        [string]$PythonVersion = '3.11',
+
+        [Parameter()]
+        [switch]$TrustedHost
     )
-    Write-Host "Creating micromamba environment: $EnvName" -ForegroundColor Yellow
 
-    # redirect output since uipath captures the output
-    & "$PSScriptRoot\micromamba" create -n $EnvName --yes python=$PythonVersion pip -c conda-forge | Out-Null
+    # Write-Host "Creating micromamba environment: $EnvName" -ForegroundColor Yellow
 
-    if ($LASTEXITCODE -eq 0 -and $Packages) {
-        Install-PackagesInMicromambaEnvironment -EnvName $EnvName -Packages $Packages
-    } elseif ($LASTEXITCODE -ne 0) {
-        Write-Host "Failed to create the micromamba environment: $EnvName" -ForegroundColor Red
+    if ($TrustedHost) {
+        # redirect output since uipath captures the output
+        & "$PSScriptRoot\micromamba" create -n $EnvName --yes --ssl-verify False python=$PythonVersion pip -c conda-forge | Out-Null
+    }
+    else {
+        # redirect output since uipath captures the output
+        & "$PSScriptRoot\micromamba" create -n $EnvName --yes python=$PythonVersion pip -c conda-forge | Out-Null
+    }
+ 
+    if ($LASTEXITCODE -eq 0) {
+        # Write-Host "Created the micromamba environment: $EnvName" -ForegroundColor Green
+        $true
+    }
+    else {
+        # Write-Host "Failed to create the micromamba environment: $EnvName" -ForegroundColor Red
+        $false
     }
 }
 
@@ -257,4 +351,4 @@ function Invoke-PythonScript {
     }
 }
 
-Export-ModuleMember -Function Invoke-THDScore, Install-PackagesInMicromambaEnvironment, Test-InputMessage, Test-MicromambaEnvironment, Initialize-MicromambaEnvironment, Invoke-PythonScript
+Export-ModuleMember -Function Initialize-MambaRootPrefix, Invoke-THDScore, Install-PackagesInMicromambaEnvironment, Test-InputMessage, Test-MicromambaEnvironment, New-MicromambaEnvironment, Invoke-PythonScript
